7fc7fbe84e27d7908349d58aa9d31c47a73c40fc
רינה קלימן < @DESKTOP-PT7G7JA>
2023-08-23 11:49:55 +0300

    finish nevigate

--- chatApp.py
+++ chatApp.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 server = Flask(__name__)
 
-@server.route('/templates/register', '/register')
-def register():
+@server.route('/register')
+def homePage():
+   return render_template('register.html')
 
-   return "register"
+@server.route('/login')
+def loginPage():
+   return render_template('login.html')
 
+# @server.route('/logout')
+# def homePage():
+#    return render_template('register.html')
+
+@server.route('/lobby')
+def room():
+   return render_template('lobby.html')
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
