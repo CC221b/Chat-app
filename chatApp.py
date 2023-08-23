3184792381c11a8b2e2de5b0184766ec5a65b923
Ayala Min Hahar <ayala59020@gmail.com>
2023-08-23 10:14:26 +0300

    Try to nevigate

--- chatApp.py
+++ chatApp.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 server = Flask(__name__)
 
-@server.route("/register")
+@server.route('/templates/register', '/register')
 def register():
+
    return "register"
 
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
+
