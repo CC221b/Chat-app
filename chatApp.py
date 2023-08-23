acca389ac1994d40c06cfe4eb8e713d4319a427f
Ayala Min Hahar <ayala59020@gmail.com>
2023-08-23 11:52:19 +0300

    start register

--- chatApp.py
+++ chatApp.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 server = Flask(__name__)
 
-@server.route('/templates/register', '/register')
+@server.route('/register' , methods=['GET', 'POST'])
 def register():
+   name = request.form('name')
+   user = {
+        "name": request.form.get('name'),
+        "password": request.form.get('password'),
+   }
 
-   return "register"
 
+@server.route('/register' , methods=['GET', 'POST'])
+def login():
+
+   username = request.args.get('username')
+   password = request.args.get('password')
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
-
