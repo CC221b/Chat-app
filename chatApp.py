a1bea5f3f98561780519cb888f2863775987a2d9
רינה קלימן < @DESKTOP-PT7G7JA>
2023-08-23 14:40:56 +0300

    merge

--- chatApp.py
+++ chatApp.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import os.path
 server = Flask(__name__)
+
 
 # @server.route('/register' , methods=['GET', 'POST'])
 # def register():
@@ -28,9 +30,18 @@ def loginPage():
 # def homePage():
 #    return render_template('register.html')
 
-@server.route('/lobby')
+@server.route('/lobby', methods =["GET", "POST"])
 def room():
+   if request.method == 'POST':
+        name = request.form["new_room"] 
+        path = './rooms/example.txt'
+        check_file = os.path.isfile(path)  
+        file1 = open(path, "w")
+        return name
    return render_template('lobby.html')
+
+   
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
+
