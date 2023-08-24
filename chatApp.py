8dd975d45e2db08ef58429306b114cf5168bb712
רינה קלימן < @DESKTOP-PT7G7JA>
2023-08-24 10:27:48 +0300

    register is work!!!!

--- chatApp.py
+++ chatApp.py
@@ -1,52 +1,53 @@
 from flask import Flask,redirect, request, render_template
 import os
-server = Flask(__name__)
 import csv
+import base64
+server = Flask(__name__)
+
+# def decode_password(encoded_password):
+#     if encoded_password:
+#        return (encoded_password).decode()
+
+# def encode_password(decoded_password):
+#     if decoded_password:
+#        return (decoded_password).encode()
 
 
-def write_to_file(data):
-   header = ['username' , 'password']
-   filename = 'users.csv'
-   with open(filename, 'w') as file:
-    for row in data:
-        for x in row:
-            file.write(str(x)+', ')
-        file.write('\n')
 
-def read_to_file(data, if_login):
-   with open('users.csv', 'r') as file:
-      reader = csv.DictReader(file)
-      for row in reader:
-         if row[0] == data.name:
-            if if_login:
-               if row[0] == data.password:
-                  return True
-            else:
-               return True
-      return False
+USERS = {}
+with open("users.csv", "r") as users_file:
+    for line in users_file:
+        parts = line.strip().split(",", 1)
+        if len(parts) == 2:
+            username, encoded_password = parts
+            password = encoded_password
+            #password = decode_password(encoded_password)
+            USERS[username] = password
 
-@server.route('/register' , methods=["GET", "POST"])
-def homePage():
-   if request.method == "POST":
-      name = request.form['username']
-      password = request.form['password']
-      data=[name, password]
-      if read_to_file(data,False):
-         return render_template("login.html")
-      else:
-         write_to_file(data)
+@server.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username not in USERS:
+            #encoded_password = encode_password(password)
+            encoded_password = password
+            USERS[username] = encoded_password
+            with open("users.csv", "a") as users_file:
+                users_file.write(f"{username},{encoded_password}\n")
+            render_template("login.html")
+    return render_template("register.html")
+
        
 
-
-
 @server.route('/login' , methods=['GET', 'POST'])
-def loginPage():
+def login():
   if request.method == 'POST':
       name = request.form['username'],
       password = request.form['password']
       data=[name, password]
       if read_to_file(data,True):
-         return render_template('lobbe.html')
+         return render_template('lobby.html')
       else:
          return render_template('register.html')
   
@@ -56,20 +57,36 @@ def loginPage():
 def room():
    if request.method == 'POST':
         new_room = request.form["new_room"] 
-        path = "rooms/" + new_room + ".txt"
-        if os.path.isfile(path):
-           return "Error in rhe room name!"
-        else:
-           with open(path, 'w') as f:
-              f.write("welcome")  
-           rooms = os.listdir('rooms/')
-               
+        if new_room:
+             path = "rooms/" + new_room + ".txt"
+             if os.path.isfile(path):
+                 return "Error in rhe room name!"
+             else:
+                 with open(path, 'w') as f:
+                     f.write("welcome")  
+   rooms = os.listdir('rooms/')         
    return render_template('lobby.html', room_names=rooms)
+
+
+
+   
+#   @app.route("/chat/<room>", methods=["GET", "POST"])
+# def chat(room):
+#     if request.method == "POST":
+#         message = request.form["msg"]
+#         if message:
+#             room_path = os.path.join(ROOMS_DIR, f"{room}.txt")
+#             with open(room_path, "a") as room_file:
+#                 timestamp = datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+#                 room_file.write(f"{timestamp} {session['username']}: {message}\n")
+#     with open(os.path.join(ROOMS_DIR, f"{room}.txt"), "r") as room_file:
+#         chat_content = room_file.read()
+#     return render_template("chat.html", room=room, chat_content=chat_content)
 
 
 
    
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0')
+   server.run(host='0.0.0.0', debug = True)
 
