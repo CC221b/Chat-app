88634ba29c8367706f5cdedff8218b8d9affebd8
רינה קלימן < @DESKTOP-PT7G7JA>
2023-08-29 15:08:40 +0300

    Finish part-2

--- chatApp.py
+++ chatApp.py
@@ -9,7 +9,8 @@ server.config["SESSION_PERMANENT"] = False
 server.config["SESSION_TYPE"] = "filesystem"
 Session(server)
 
-ROOMS_DIR = os.getenv("ROOMS_DIR", "rooms") 
+rooms_dir = os.getenv("ROOMS_PATH") 
+users_path = os.getenv("USERS_PATH")
 
 def decode_password(encoded_password):
     try:
@@ -26,7 +27,7 @@ def encode_password(decoded_password):
     return base64_bytes.decode('ascii')
 
 USERS = {}
-with open("users.csv", "r") as users_file:
+with open(users_path, "r") as users_file:
     for line in users_file: 
         parts = line.strip().split(",", 1)
         if len(parts) == 2:
@@ -42,7 +43,7 @@ def register():
         if username not in USERS:
             encoded_password = encode_password(password)
             USERS[username] = password
-            with open("users.csv", "a") as users_file:
+            with open(users_path, "a") as users_file:
                 users_file.write(f"{username},{encoded_password}\n")
         else:
             return "username already exists!!"
@@ -67,7 +68,7 @@ def room():
    if request.method == 'POST':
         new_room = request.form["new_room"] 
         if new_room:
-             room_path = os.path.join(ROOMS_DIR, f"{new_room}.txt")
+             room_path = os.path.join(rooms_dir, f"{new_room}.txt")
              if os.path.isfile(room_path):
                  return "Error in rhe room name!"
              else:
@@ -96,11 +97,11 @@ def chat_room(room):
    if request.method == "POST":
         message = request.form["msg"]
         if message:
-            room_path = os.path.join(ROOMS_DIR, f"{room}")
+            room_path = os.path.join(rooms_dir, f"{room}")
             with open(room_path, "a") as room_file:
                 timestamp = datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
                 room_file.write(f"{timestamp} {session.get('username')}: {message}\n")
-   with open(os.path.join(ROOMS_DIR, f"{room}"), "r") as room_file:
+   with open(os.path.join(rooms_dir, f"{room}"), "r") as room_file:
       room_file.seek(0)
       chat_content = room_file.read()
    return chat_content
--- thin.dockerfile
+++ thin.dockerfile
@@ -1,5 +1,5 @@
 # set base image (host OS)
-FROM python:3.8
+FROM python:3.8-slim AS reduce_docker_image
 
 # set the working directory in the container
 WORKDIR /code
@@ -8,14 +8,16 @@ WORKDIR /code
 COPY requirements.txt .
 
 # install dependencies
-RUN pip install -r requirements.txt
+RUN pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r requirements.txt
+
+FROM reduce_docker_image
+
+# copy the content of the local src directory to the working directory
+COPY --from=reduce_docker_image /code /code
 
 ENV FLASK_ENV development
 ENV ROOMS_PATH rooms/
 ENV USERS_PATH users.csv
-
-# copy the content of the local src directory to the working directory
-COPY / .
 
 # command to run on container start
 CMD [ "python", "./chatApp.py" ]
