da92fa99e1a982e2fc67b25c18e2b7483965a7b6
רינה קלימן < @DESKTOP-PT7G7JA>
2023-08-30 09:24:32 +0300

    v0.0.0

--- delete.sh
+++ delete.sh
@@ -5,11 +5,4 @@ docker rm -f $(docker ps -a -q)
 # Deleting all images
 docker rmi -f $(docker images -aq)
 
-# Deleting all containers
-#docker rm -f $(docker rm -aq)
 
-
-# Personal Automation use
-
-# docker rmi -f  <image name>
-# docker rm -f <container name>
